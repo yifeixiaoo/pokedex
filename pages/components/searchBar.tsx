401a950cc8c9f3aaa273e987d165ba57e6662de1
Yifei Xiao <yifeixiaoo@gmail.com>
2023-05-06 00:42:28 -0600

    import material ui search bar

--- pages/components/searchBar.tsx
+++ pages/components/searchBar.tsx
@@ -1,7 +1,19 @@
+import { Box } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField/TextField";
+
 export default function SearchBar() {
     return (
-      <div>
-        SearchBar
-      </div>
+        <Box
+        // component="form"
+        // sx={{
+        //   '& > :not(style)': { m: 1, width: '25ch' },
+        // }}
+        // noValidate
+        // autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+      </Box>
     );
   }
