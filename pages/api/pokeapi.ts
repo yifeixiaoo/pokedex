38033538322860799d3890afd0ec1ad371f24cbd
Yifei Xiao <yifeixiaoo@gmail.com>
2023-05-05 23:52:29 -0600

    add searchbar

--- pages/api/pokeapi.ts
+++ pages/api/pokeapi.ts
@@ -4,4 +4,6 @@ import { GetServerSideProps } from "next";
 async function getPokemon() {
     const pokemonURL = 'https://pokeapi.co/api/v2/pokemon/1';
     const pokemonResponse = await fetch(pokemonURL);
+    const pokemonData = await pokemonResponse.json();
+    
 }
